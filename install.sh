950f7bd351e71ff49cb3aa3c674cdcff7711a2eb
Scott Griepentrog <scott@griepentrog.com>
2012-09-10 08:17:18 -0400

    added 32 bit support to installer

--- install.sh
+++ install.sh
@@ -1,28 +1,49 @@
 #!/bin/sh
 
-if [ ! -d processing-2.0b1 ]
+if [ 'uname -i' = 'x86_64' ]
 then
-	wget -O - http://processing.googlecode.com/files/processing-2.0b1-linux64.tgz |tar xvfz -
+	PLATFORM="x64"
+	BITS="64"
+else
+	PLATFORM="x86"
+	BITS="32"
 fi
 
-if [ ! -d OpenNI_NITE_Installer-Linux64-0.27 ]
+PROCESSING_URL="http://processing.googlecode.com/files/processing-2.0b1-linux$BITS.tgz"
+PROCESSING="processing-2.0b1"
+
+NITE_URL="http://simple-openni.googlecode.com/files/OpenNI_NITE_Installer-Linux$BITS-0.27.zip"
+NITE="OpenNI_NITE_Installer-Linux$BITS-0.27"
+
+SIMPLE_URL="http://simple-openni.googlecode.com/files/SimpleOpenNI-0.27.zip"
+
+if [ ! -d $PROCESSING ]
 then
-	wget -O OpenNI_Installer.zip http://simple-openni.googlecode.com/files/OpenNI_NITE_Installer-Linux64-0.27.zip
+	wget -O - $PROCESSING_URL |tar xvfz -
+fi
+
+if [ ! -d $NITE ]
+then
+	wget -O OpenNI_Installer.zip $NITE_URL
 	unzip OpenNI_Installer.zip
 
-	(cd OpenNI_NITE_Installer-Linux64-0.27/NITE-Bin-Dev-Linux-x64-v1.5.2.21; sudo ./install.sh)
-	(cd OpenNI_NITE_Installer-Linux64-0.27/OpenNI-Bin-Dev-Linux-x64-v1.5.4.0; sudo ./install.sh)
+	(cd $NITE/NITE-Bin-Dev-Linux-$PLATFORM-v1.5.2.21; sudo ./install.sh)
+	(cd $NITE/OpenNI-Bin-Dev-Linux-$PLATFORM-v1.5.4.0; sudo ./install.sh)
 
-	#OpenNI_NITE_Installer-Linux64-0.27/Sensor-Bin-Linux-x64-v5.1.2.1/install.sh
-	#OpenNI_NITE_Installer-Linux64-0.27/kinect/Sensor-Bin-Linux-x64-v5.1.2.1/install.sh
+	#$NITE/Sensor-Bin-Linux-x64-v5.1.2.1/install.sh
+	#$NITE/kinect/Sensor-Bin-Linux-x64-v5.1.2.1/install.sh
 fi
 
 
-if [ ! -f SimpleOpenNI-0.27.zip ]
+SIMPLE_ZIP="SimpleOpenNI.zip"
+
+if [ ! -f $SIMPLE_ZIP ]
 then
-	wget -O SimpleOpenNI-0.27.zip http://simple-openni.googlecode.com/files/SimpleOpenNI-0.27.zip
+	wget -O $SIMPLE_ZIP $SIMPLE_URL
 
-	mkdir -p processing-2.0b1/sketchbook/libraries
-	(cd processing-2.0b1/sketchbook/libraries; ls ../../..;unzip ../../../SimpleOpenNI-0.27.zip)
+	mkdir -p $PROCESSING/sketchbook/libraries
+	(cd $PROCESSING/sketchbook/libraries; ls ../../..;unzip ../../../$SIMPLE_ZIP)
 fi
+
+./$PROCESSING/processing rockem.pde
 
